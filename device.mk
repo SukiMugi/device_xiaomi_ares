659890098e04bb671a065a2d9686aa830be189a4
jumitenei <134821284+jumitenei@users.noreply.github.com>
2024-06-02 13:38:26 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -65,14 +65,12 @@ PRODUCT_PACKAGES += \
 TARGET_RECOVERY_DEVICE_MODULES += \
     android.hardware.keymaster@4.1 \
     libkeymaster4 \
-    libpuresoftkeymasterdevice \
-    android.hardware.keymaster@4.0
+    libpuresoftkeymasterdevice
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.0.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1.so
 
 PRODUCT_PACKAGES += \
     android.system.keystore2
