805b95a6746baf091117ff3121a9a79f5b873562
jumitenei <134821284+jumitenei@users.noreply.github.com>
2024-07-02 16:15:15 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2021 The TWRP Open Source Project
+# Copyright (C) 2022 The OrangeFox Recovery Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,58 +14,63 @@
 # limitations under the License.
 #
 
-# API
-PRODUCT_SHIPPING_API_LEVEL := 31
-
-# Dynamic
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
+# Virtual A/B
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
-# A/B
-ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+LOCAL_PATH := device/xiaomi/ares
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 31
 
 # A/B
-AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    system \
-    system_ext \
-    product \
-    vendor \
-    odm \
-    vbmeta \
-    vbmeta_system \
-    vbmeta_vendor
+PRODUCT_PACKAGES += \
+    otapreopt_script
 
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot Control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-impl
+
+# Update Engine
 PRODUCT_PACKAGES += \
     update_engine \
     update_engine_sideload \
     update_verifier
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
 
-# Boot control HAL
+# Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl.recovery
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock
 
-# fastbootd
+# Health HAL
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
-    
-# Additional target Libraries
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl
+
+# Additional Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
     libpuresoftkeymasterdevice
 
-TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
